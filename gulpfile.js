776cb318bc588bc067af027289c6392ca9b0fdbf
fengkx <liangkx8237@gmail.com>
2020-03-16 00:04:36 +0800

    p2(fail): use babel and preset-env, fail ReferenceError: regeneratorRuntime is not defined

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 const gulp = require('gulp');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
 
 gulp.task('js', () => {
     return gulp.src('main.js')
+    .pipe(babel({
+        presets: ['@babel/preset-env'],
+    }))
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('.'));

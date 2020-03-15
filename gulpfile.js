c2de149d219363f2e4fed1a47520e0435b3e5037
fengkx <liangkx8237@gmail.com>
2020-03-16 00:26:49 +0800

    p4(fail): fail gulp use vinyl internal

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,16 @@ const gulp = require('gulp');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
+const browserify = require('browserify');
 
 gulp.task('js', () => {
-    return gulp.src('main.js')
-    .pipe(babel({
+    return browserify("main.js")
+    .transform("babelify", {
         presets: [
             ['@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 }]
         ],
-    }))
+    })
+    .bundle()
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('.'));

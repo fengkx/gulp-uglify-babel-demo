fc7ecdc41dd4ce05329ee090de329ca2265040fd
fengkx <liangkx8237@gmail.com>
2020-03-16 00:34:33 +0800

    p5(success): successfully run at browser

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
 const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
 
 gulp.task('js', () => {
     return browserify("main.js")
@@ -12,6 +14,8 @@ gulp.task('js', () => {
         ],
     })
     .bundle()
+    .pipe(source('main.js'))
+    .pipe(buffer())
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('.'));
